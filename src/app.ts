58f321c3502884845551df39b159943d9e3b3a27
Joseph Moran <josephdeargmoran@protonmail.com>
2023-03-12 22:11:21 -0400

    Cors fix

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,18 @@ import Database from "./Databse"
 import APIRouter from "./routes/api"
 import bodyParser from "body-parser"
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
 //Make sure I'm connected to the database and all that
 const start = async ():Promise<void> => {
   dotenv.config();
   await Database.connect();
+  app.use(cors());
   app.use(express.json());
   app.use(bodyParser.json())
   app.use("/api", APIRouter);
